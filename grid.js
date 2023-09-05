495901987df24c82edb24926a3957611d0b86679
gKyle1109 <kyleholmes1119@gmail.com>
2023-09-05 13:40:52 -0700

    adding the drawing feture

--- grid.js
+++ grid.js
@@ -1,5 +1,4 @@
 const mainGrid = document.getElementById("container");
-let color = "black"
 const reset = document.getElementById("Reset");
 function createGrid() {
     mainGrid.textContent = ""
@@ -12,4 +11,15 @@ function createGrid() {
 reset.addEventListener("click", createGrid)
 let cell = mainGrid.children
 
-
+const white = document.getElementById("White")
+const black = document.getElementById("Black")
+const setcolor = document.getElementById("set")
+let color = "black"
+white.addEventListener("click", () => color = "white")
+black.addEventListener("click", () => color = "black")
+setcolor.addEventListener("click", colorCode)
+function colorCode() {
+    for (const block of cell) {
+        block.addEventListener("mouseover", () => block.style.backgroundColor = color)
+    }
+}
